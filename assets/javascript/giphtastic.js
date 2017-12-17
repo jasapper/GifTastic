04d90a3ee06a08381f1fdee2958960d13b7e006a
jasapper <jasap98@yahoo.com>
2017-12-17 06:55:51 -0500

    JS code should be finished, advanced HTML and CSS still to do

--- assets/javascript/giphtastic.js
+++ assets/javascript/giphtastic.js
@@ -1,13 +1,93 @@
-// Define global variables
+$(document).ready(function(){
 
 // Default topic array
+ var subject = ["Spaceballs", "The Matrix", "Fifth Element", "Jason Bourne", "Full Metal Jacket", "Independence Day", "Mel Brooks"];
 
-// Button click function (still <-> animated)
+// Create buttons for default subjects
+function buttonCreate(){
+    $('#button-panel').empty();
+    
+    for ( var i=0; i < subject.length; i++) {
+
+        var a = $('<button>');
+        a.addClass('buttonClick');
+        a.attr('data-name', subject[i]);
+        a.text(subject[i]);
+        $('#button-panel').append(a);
+    }
+}    
+buttonCreate();
+
+
+
+$(document).on('click', '.buttonClick', function() {
+
+var buttonSubject = $(this).html(); 
+console.log(buttonSubject);
 
 // Ajax and API call(s)
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonSubject + "&api_key=QKBZvWiPgbaEHCWME3tpqkV5lzihvNhn&limit=10";
+    // console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .done(function(response) {
+        var results = response.data;
+        $('#giph-panel').empty();
+            // For Loop(ing) through results
+            for ( var j=0; j < results.length; j++) {
+                var giphyDiv = $('<div>');
+                var giphyURL = results[j].images.fixed_height.url;
+                var still = results[j].images.fixed_height_still.url;
+                    // console.log(giphyURL);  
+                var imageSubject = $('<img>').attr("src", still).attr('data-animate', giphyURL).attr('data-still', still);
+                imageSubject.attr('data-state', 'still');
+                $('#giph-panel').prepend(imageSubject);
+                imageSubject.on('click', stateClick);
+                
+                // Rating logic (limiting?)
+                    var rating = results[j].rating;
+                        // console.log(rating);
+                    var showRating= $('<p>').text("Rating: " + rating);
+                    $('#giph-panel').prepend(showRating);
+        
+            }
+    });
 
-// For Loop(ing) through results
+    // Button click function (still <-> animated)
+    function stateClick() { 
+                var state = $(this).attr('data-state');
+                console.log(state);
+             if ( state == 'still'){
+                 $(this).attr('src', $(this).data('animate'));
+                  $(this).attr('data-state', 'animate');
+             } else{
+                 $(this).attr('src', $(this).data('still'));
+                 $(this).attr('data-state', 'still');
+                }
+
+            }
+            
+})
+
+   
+
 
 // Adding new button(s)
+$(document).on('click', '#addSubject', function(){
+if ($('#subject-input').val().trim() == ''){
+  alert('Input can not be left blank');
+}
+else {
+var buttonSubject = $('#subject-input').val().trim();
+subject.push(buttonSubject);
+$('#subject-input').val('');
+buttonCreate();
+return false;
 
-// Rating logic (limiting?)
+}
+
+});
+
+});
