b5ba6efea3484b6bab74ef80b909898067596db4
jasapper <jasap98@yahoo.com>
2017-12-17 01:37:52 -0500

    JS outline and adding script/css links

---
+++ assets/javascript/giphtastic.js
@@ -0,0 +1,13 @@
+// Define global variables
+
+// Default topic array
+
+// Button click function (still <-> animated)
+
+// Ajax and API call(s)
+
+// For Loop(ing) through results
+
+// Adding new button(s)
+
+// Rating logic (limiting?)
